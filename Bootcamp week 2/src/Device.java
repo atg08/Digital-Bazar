bef4d8a749c70f3686b6765fd9cdb1d6dd7b977d
AdittiGupta <agup0031@student.monash.edu>
2023-03-16 13:02:50 +0800

    Bootcamp week 2 files

--- Bootcamp week 2/src/Device.java
+++ Bootcamp week 2/src/Device.java
@@ -1,30 +1,41 @@
 public class Device {
 
+    //string attributes
+
     private String name;
     private String description;
 
+    //constructor with two parameters
     public Device(String name,String description) {
         setName(name);
         setDescription(description);
     }
 
-
+    //setter setName
     public void setName(String name) {
+
         this.name = name;
     }
-
+    //getter getName
     public String getName() {
+
         return name;
     }
 
+    //setter setDescription
     public void setDescription(String description) {
+
         this.description = description;
     }
 
+    //getter getDescription
     public String getDescription() {
+
         return description;
     }
 
+
+    //toString method
     @Override
     public String toString() {
         return " | name: " + getName()+
--- Bootcamp week 2/src/Printer.java
+++ Bootcamp week 2/src/Printer.java
@@ -1,23 +1,30 @@
 public class Printer extends Device {
 
+    //string attribute
     private int ppm;
 
+    //constructor
     public Printer(String name, String description, int ppm) {
         super(name, description);
         setPpm(ppm);
     }
 
+    //setter setPpm
     public void setPpm(int ppm) {
+
         this.ppm = ppm;
     }
 
+    //getter getPpm
     public int getPpm() {
+
         return ppm;
     }
 
+    //toString method
     @Override
     public String toString() {
         return "Printer" + super.toString()+
-                " | ppm=" + ppm;
+                " | ppm=" + getPpm();
     }
 }
